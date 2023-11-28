6ea8de43efce7388e04acbdc03be8fbf5121188d
SiRanDev <siranweb.public@tuta.io>
2023-11-28 18:56:33 +0300

    clean up & node upgrade

--- applications/backend/src/infra/common/global-imports.ts
+++ applications/backend/src/infra/common/global-imports.ts
@@ -1,8 +1,3 @@
 // This file can be imported in your entry files if you need
-
 import 'dotenv/config';
 import 'reflect-metadata';
-
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-dayjs.extend(utc);
