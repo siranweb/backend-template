05749542327da1315b98c320f125736509f8075d
SiRanDev <siranweb.public@tuta.io>
2023-12-08 15:07:30 +0300

    register

--- applications/backend/src/app/users/auth/gateway/controllers/accounts.controller.ts
+++ applications/backend/src/app/users/auth/gateway/controllers/accounts.controller.ts
@@ -26,37 +26,4 @@ export class AccountsController {
     ctx.res.setHeader('Set-Cookie', cookie);
     ctx.res.end();
   }
-
-  // @Endpoint('GET', '/:id')
-  // async getUser(req: Request, res: Response, params: Params) {
-  //   const parsed = getUserSchema.parse({ params });
-  //   const user = {
-  //     id: parsed.params.id,
-  //   };
-  //   // throw new UserNotFoundError();
-  //
-  //   res.end(JSON.stringify({ user }));
-  // }
-  // @Endpoint('GET', '/:id')
-  // async getUser(ctx: Koa.Context) {
-  //   // ctx.params.id = '';
-  //   const parsed = getUserSchema.parse(ctx);
-  //   // throw new UserNotFoundError();
-  //   // throw new Error('');
-  //   const user = {
-  //     id: parsed.params.id,
-  //   };
-  //
-  //   return {
-  //     user,
-  //   };
-  // }
-  //
-  // @Endpoint('POST', '/')
-  // async createUser(ctx: Koa.Context) {
-  //   const user = {
-  //     id: 100,
-  //   };
-  //   ctx.body = { user };
-  // }
 }
