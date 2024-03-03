79bb65130ab123ca40b41d438c59f89c3bd63a50
SiranWeb <kirillg.web@gmail.com>
2024-03-03 15:47:08 +0300

    fix: query

--- applications/backend/src/app/users/shared/users.repository.ts
+++ applications/backend/src/app/users/shared/users.repository.ts
@@ -6,15 +6,13 @@ export class UsersRepository implements IUsersRepository {
   constructor(private readonly db: AppDatabase) {}
 
   async saveAccount(account: Account): Promise<Account> {
-    return await this.db.transaction().execute(async (trx): Promise<Account> => {
-      const result = await trx
+    const result = await this.db
         .insertInto('account')
         .values(account)
         .returningAll()
         .executeTakeFirstOrThrow();
 
-      return new Account(result);
-    });
+    return new Account(result);
   }
 
   async getAccountByLogin(login: string): Promise<Account | null> {
