6048aab26ff80aca1989d5c92dfd68aa6c2dcdaa
siranweb <siranweb.public@tuta.io>
2024-05-25 13:35:30 +0300

    feat: scheduler

--- app/index.ts
+++ app/index.ts
@@ -3,12 +3,15 @@ import { appDatabase } from '@/infrastructure/app-database/database';
 import { startServer, stopServer } from '@/infrastructure/web-server';
 import { config } from '@/infrastructure/config';
 import { makeLogger } from '@/infrastructure/logger/make-logger';
+import { startScheduler } from '@/infrastructure/scheduler';
 
 const logger = makeLogger('App');
 
 startServer(config.webServer.port).then(() =>
   logger.info(`Web server started on port ${config.webServer.port}.`),
 );
+
+startScheduler().then(() => logger.info('Scheduler started.'));
 
 const shutdown = () => {
   Promise.allSettled([stopServer(), appDatabase.destroy()]).finally(() => process.exit(0));
---
+++ app/infrastructure/scheduler/index.ts
@@ -0,0 +1,14 @@
+import { Scheduler } from '@/lib/scheduler';
+import { makeLogger } from '@/infrastructure/logger/make-logger';
+
+const logger = makeLogger(Scheduler.name);
+const scheduler = new Scheduler();
+scheduler.onTrigger((ctx) =>
+  logger.info(`${ctx.name} was launched on ${new Date(ctx.timestamp).toLocaleString()}`),
+);
+
+export async function startScheduler() {
+  scheduler.add('* * * * * *', 'Test scheduler', () => {
+    console.log('I am test scheduler');
+  });
+}
